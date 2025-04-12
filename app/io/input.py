4348d2ee7cc4701fd0e2c558fd44c3fe361fd407
Viktoriia Dovhan <v.dovhan@ukma.edu.ua>
2025-04-12 16:34:50 +0300

    Create empty functions in io

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def read_from_console():
+    pass
+
+
+def read_from_file_with_python():
+    pass
+
+
+def read_from_file_with_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def write_in_console():
+    pass
+
+
+def write_in_file_with_python():
+    pass
+
+
+def write_in_file_with_pandas():
+    pass
